d075be11161ade717a4c1825169a90c4dc1db118
Guilherme Borges <g.borges@campus.fct.unl.pt>
2018-06-24 17:29:23 +0100

    integrated multi threaded mbedtls server into App.cpp

--- Framework/src/IeeComm/App.cpp
+++ Framework/src/IeeComm/App.cpp
@@ -5,27 +5,41 @@
 
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <trusted_mbedtls.h>
 
 #include "mbedtls/ssl.h"
 #include "mbedtls/net.h"
 #include "mbedtls/error.h"
 
-#include "server.h" // TODO remove
+#include "mbedtls_net.c"
 
 // to pass data to client thread
 typedef struct client_data {
     sgx_enclave_id_t eid;
-    int socket;
+    thread_info_t* info;
 } client_data;
 
-static int server_socket;
+static mbedtls_net_context listen_fd;
 
 static void close_all(int signum) {
-    close(server_socket);
+    mbedtls_net_free(&listen_fd);
     fflush(NULL);
     exit(0);
 }
 
+void* ecall_handle_ssl_connection(void* data) {
+    long thread_id = (long)pthread_self();
+
+    client_data* cdata = (client_data*)data;
+    ssl_conn_handle(cdata->eid, thread_id, cdata->info);
+
+    printf("thread exiting for thread %ld\n", thread_id);
+    mbedtls_net_free(&cdata->info->client_fd);
+    return NULL;
+}
+
+/*
+ DEPRECATED, logic goes into enclave
 void* process_client(void* args) {
     const int socket = ((client_data *)args)->socket;
     sgx_enclave_id_t eid = ((client_data *)args)->eid;
@@ -60,7 +74,7 @@ void* process_client(void* args) {
 
     printf("Client closed\n");
     close(socket);
-}
+}*/
 
 int SGX_CDECL main(int argc, const char **argv) {
     // port to start the server on
@@ -88,35 +102,68 @@ int SGX_CDECL main(int argc, const char **argv) {
         exit(-1);
     }
 
-    dostuff(eid); // TODO pass that stuff to here
-
     // initialise thread pool
     thread_pool* pool = init_thread_pool(eid, nr_threads);
 
-    // initialise listener socket
-    server_socket = untrusted_util::init_server(server_port);
+    // mbedtls
+    ssl_conn_init(eid);
 
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len = 0;
+    printf("  . Bind on https://localhost:4433/ ...");
+    fflush(stdout);
 
-    printf("Listening for requests...\n");
-    while (1) {
-        client_data *data = (client_data *)malloc(sizeof(client_data));
-        data->eid = eid;
+    int ret;
+    if ((ret = mbedtls_net_bind(&listen_fd, NULL, "4433", MBEDTLS_NET_PROTO_TCP)) != 0) {
+        printf(" failed\n  ! mbedtls_net_bind returned %d\n\n", ret);
+        exit(-1);
+    }
 
-        if ((data->socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len)) < 0) {
-            printf("Accept failed!\n");
-            //exit(1);
+    printf(" ok\n");
+    printf("  [ main ]  Waiting for a remote connection\n");
+
+    while (true) {
+#ifdef MBEDTLS_ERROR_C
+        if (ret != 0) {
+            char error_buf[100];
+            mbedtls_strerror(ret, error_buf, 100);
+            printf("  [ main ]  Last error was: -0x%04x - %s\n", -ret, error_buf);
+        }
+#endif
+
+        // init thread data
+        client_data* data = (client_data*)malloc(sizeof(client_data));
+        memcpy(&data->eid, &eid, sizeof(sgx_enclave_id_t));
+        data->info = (thread_info_t*)malloc(sizeof(thread_info_t));
+        data->info->config = NULL;
+        data->info->thread_complete = 0;
+
+        // 3. Wait until a client connects
+        if (0 != mbedtls_net_set_nonblock(&listen_fd))
+            printf("can't set nonblock for the listen socket\n");
+
+        mbedtls_net_context client_fd;
+        ret = mbedtls_net_accept(&listen_fd, &client_fd, NULL, 0, NULL);
+        if (ret == MBEDTLS_ERR_SSL_WANT_READ) {
+            ret = 0;
+            continue;
+        } else if (ret != 0) {
+            printf("  [ main ] failed: mbedtls_net_accept returned -0x%04x\n", ret);
             break;
         }
 
-        printf("Client connected (%s)\n", inet_ntoa(client_addr.sin_addr));
+        printf("  [ main ]  ok\n");
+        printf("  [ main ]  Creating a new thread for client %d\n", client_fd.fd);
+
+        memcpy(&data->info->client_fd, &client_fd, sizeof(mbedtls_net_context));
 
         pthread_t tid;
-        pthread_create(&tid, NULL, process_client, data);
+        if ((ret = pthread_create(&tid, NULL, ecall_handle_ssl_connection, data)) != 0) {
+            printf("  [ main ]  failed: thread_create returned %d\n", ret);
+            mbedtls_net_free(&client_fd);
+            continue;
+        }
     }
 
-    close(server_socket);
+    mbedtls_net_free(&listen_fd);
 
     // cleanup and exit
     destroy_enclave(eid);
--- Framework/src/IeeComm/server.h
+++
@@ -1,9 +0,0 @@
-#ifndef SGX_IMAGE_SEARCH_MSERVERDEBUG_H
-#define SGX_IMAGE_SEARCH_MSERVERDEBUG_H
-
-#include "sgx_eid.h"
-#include "sgx_urts.h"
-
-int dostuff(sgx_enclave_id_t eid);
-
-#endif
